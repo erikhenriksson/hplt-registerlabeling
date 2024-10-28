5049be09bfb3bcde21bd4ebdf75f94b2ccd2ac4f
Erik <eekihenriksson@gmail.com>
2024-10-28 17:39:02 +0200

    Update predict2.py

--- predict2.py
+++ predict2.py
@@ -165,8 +165,8 @@ def process_and_save_ddp(rank, cfg, world_size):
 # Main function to run with torch.multiprocessing.spawn
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--max_batch_length", type=int, default=2000)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--max_batch_length", type=int, default=1000)
     parser.add_argument("--model_path", default="models/xlm-roberta-base")
     parser.add_argument("--input_path", default="data/en/1_sample.jsonl.zst")
     parser.add_argument(
