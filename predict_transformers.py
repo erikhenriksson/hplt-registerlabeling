dcc6970cf8c73783d7cf1e94c7d63d9752b23203
Erik <eekihenriksson@gmail.com>
2024-10-31 00:57:14 +0200

    Update predict_transformers.py

--- predict_transformers.py
+++ predict_transformers.py
@@ -176,7 +176,7 @@ def save_results(results: List[Dict], output_path: str, mode: str = "w"):
 
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "12354"
+    os.environ["MASTER_PORT"] = "12356"
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
 
